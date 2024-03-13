712332440181415a496177376ffe8a2b2a42def8
Avishek <avishek.kdh@gmail.com>
2024-03-13 12:04:17 +0530

    navbar update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-let nav = document.querySelector("nav");
-window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
-    nav.classList.add("sticky");
-  }else {
-    nav.classList.remove("sticky");
-  }
-}
+import "../node_modules/bootstrap/dist/js";
